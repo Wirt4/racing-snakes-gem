dabf3bbb34082a7f6a495f861371d6465fcceae1
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-23 21:30:26 -0700

    red

--- spec/racing_snakes_cli_spec.rb
+++ spec/racing_snakes_cli_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe RacingSnakes::CLI do
       described_class.run
     end
   end
+  describe ".run" do
+    it"calls start_game_loop on the engine instance" do
+      engine_double = instance_double(RacingSnakes::Engine)
+      expect(engine_double).to receive(:start_game_loop)
+      described_class.run
+    end
+  end
 end
-
