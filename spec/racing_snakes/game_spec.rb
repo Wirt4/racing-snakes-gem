2c7763283acc17c60ffde94380c55c63f7449f37
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-24 16:56:25 -0700

    red

---
+++ spec/racing_snakes/game_spec.rb
@@ -0,0 +1,8 @@
+require 'racing_snakes_gem'
+RSpec.describe RacingSnakes::Game do
+  describe '#initialize'do
+    it 'initalizes a new clock object'do
+      expect(true).to equal(false)
+    end
+  end
+end
