fa61cafe7ae8b68057609222237628f954efddb1
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-24 14:11:27 -0700

    green

---
+++ spec/support/shared_contexts.rb
@@ -0,0 +1,24 @@
+# spec/support/shared_contexts.rb
+RSpec.shared_context 'window mock' do
+  let(:mock_window) { instance_double(Ruby2D::Window, set: nil, clear: nil) }
+end
+
+RSpec.shared_context 'game mock' do
+  let(:mock_game) do
+    instance_double(
+      RacingSnakes::Game,
+      move: nil,
+      draw_snakes: nil,
+      paused?: false,
+      draw_board: nil,
+      player_eats?: false,
+      eat_and_grow: nil,
+      food_time?: false,
+      respawn_food: nil,
+      is_collision?: false,
+      stop_game: nil
+    )
+  end
+  let(:mock_game){game}
+end
+
