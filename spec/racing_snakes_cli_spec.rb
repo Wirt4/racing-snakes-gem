777b2ed7bff0f1acc95fb87f82f24cb6a4222f93
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-23 21:40:18 -0700

    removed duplication

--- spec/racing_snakes_cli_spec.rb
+++ spec/racing_snakes_cli_spec.rb
@@ -1,19 +1,21 @@
 require "racing_snakes_gem"
 
 RSpec.describe RacingSnakes::CLI do
+
   describe ".run" do
+    let(:engine_double) {instance_double(RacingSnakes::Engine)}
+    before do
+      allow(RacingSnakes::Engine).to receive(:new).and_return(engine_double)
+    end
+
     it "initializes an engine" do
-      engine_double = instance_double(RacingSnakes::Engine)
       allow(engine_double).to receive(:start_game_loop)
       expect(RacingSnakes::Engine).to receive(:new).and_return(engine_double)
 
       described_class.run
     end
-  end
-  describe ".run" do
+
     it"calls start_game_loop on the engine instance" do
-      engine_double = instance_double(RacingSnakes::Engine)
-      allow(RacingSnakes::Engine).to receive(:new).and_return(engine_double)
       expect(engine_double).to receive(:start_game_loop)
       described_class.run
     end
