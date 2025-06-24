458e4dba088d89826084501e5451f20a5451bf68
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-23 21:08:23 -0700

    red

---
+++ spec/racing_snakes_cli_spec.rb
@@ -0,0 +1,14 @@
+require "racing_snakes_gem"
+
+RSpec.describe RacingSnakes::CLI do
+  describe ".run" do
+    it "initializes an engine and starts the game loop" do
+      engine_double = instance_double(RacingSnakes::Engine)
+
+      expect(RacingSnakes::Engine).to receive(:new).and_return(engine_double)
+
+      described_class.run
+    end
+  end
+end
+
--- spec/racing_snakes_gem_spec.rb
+++ spec/racing_snakes_gem_spec.rb
@@ -5,9 +5,3 @@ RSpec.describe RacingSnakesGem do
     expect(RacingSnakesGem::VERSION).not_to be nil
   end
 end
-
-Repspec.describe RacingSnakesGem do
-  it "Run creates a new game object" do
-    expect(True).to_be False
-  end
-end
