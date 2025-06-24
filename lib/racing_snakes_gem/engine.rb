7d82824fca48a9a446bd0193159d223e27f36edb
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-23 22:00:25 -0700

    red

--- lib/racing_snakes_gem/engine.rb
+++ lib/racing_snakes_gem/engine.rb
@@ -1,6 +1,8 @@
 module RacingSnakes
   class Engine
     def start_game_loop
+      def initialize
+        put "stub intializer"
     # stub
     end
   end
---
+++ spec/racing_snakes_engine.rb
@@ -0,0 +1,39 @@
+require "racing_snakes_gem"
+
+RSpec.describe RacingSnakes::Engine do
+  describe "#initialize" do
+    let(:mock_game) { double("Game") }
+    let(:mock_window) { double("Ruby2D", set: nil) }
+    let(:engine) { described_class.new(window_adapter: mock_window) }
+
+
+    before do
+      stub_const("Settings::BACKGROUND", "black")
+      stub_const("Settings::WIDTH", 640)
+      stub_const("Settings::HEIGHT", 480)
+      stub_const("Settings::FPS", 60)
+      stub_const("Settings::FULLSCREEN", false)
+      allow(Game).to receive(:new).and_return(mock_game)
+    end
+
+    it "engine has a defined Game to manipulate"do
+      expect(engine.game).not_to be_nil
+    end
+
+    it "sets up the game window using Settings" do
+      expect(mock_window).to receive(:set).with(background: "black")
+      expect(mock_window).to receive(:set).with(width: 640)
+      expect(mock_window).to receive(:set).with(height: 480)
+      expect(mock_window).to receive(:set).with(fps_cap: 60)
+      expect(mock_window).to receive(:set).with(fullscreen: false)
+
+      described_class.new(window_adapter: mock_window)
+    end
+
+    it "stores the game instance" do
+      engine = described_class.new(window_adapter: mock_window)
+      expect(engine.game).to eq(mock_game)
+    end
+  end
+end
+
