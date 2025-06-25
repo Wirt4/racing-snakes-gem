be0bb01d938ef6fe9d4f43ea72e6a4e6c17b9ca0
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-25 16:43:31 -0700

    red

--- spec/racing_snakes/clock_spec.rb
+++ spec/racing_snakes/clock_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe RacingSnakes::Clock do
       expect(loaded_clock.time).to eq(7)
     end
   end
+  describe '#tick'do
+    it 'increment from 0 to 1' do
+      clock.tick
+      expect(clock.time).to eq(1)
+    end
+
+    it 'increment from 7 to 8' do
+            loaded_clock = described_class.new(7)
+
+      loaded_clock.tick
+      expect(loaded_clock.time).to eq(8)
+    end
+  end
+
 end
