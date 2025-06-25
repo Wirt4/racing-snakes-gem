142e5ddac6c16e590ad81ff45876e5443b734167
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-24 19:08:44 -0700

    the red I want

--- lib/racing_snakes_gem/classes/board.rb
+++ lib/racing_snakes_gem/classes/board.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module RacingSnakes
+  attr_reader :snake1, :snake2
   class Board
+    def initialize(snake1, snake2)
+      @snake1 = snake1
+      @snake2 = snake2
+    end
   end
 end
--- lib/racing_snakes_gem/classes/game.rb
+++ lib/racing_snakes_gem/classes/game.rb
@@ -3,13 +3,13 @@
 module RacingSnakes
   # Game object for racing snakes, holds and maintains state
   class Game
-    attr_reader :clock, :snake_1, :snake_2, :board
+    attr_reader :clock, :player1, :player2, :board
 
     def initialize
       @clock = RacingSnakes::Clock.new
-      @snake_1 = RacingSnakes::Snake.new(RacingSnakes::PlayerIds::PLAYER_ONE)
-      @snake_2 = RacingSnakes::Snake.new(RacingSnakes::PlayerIds::PLAYER_TWO)
-      @board = RacingSnakes::Board.new(@snake_1, @snake_2)
+      @player1 = RacingSnakes::Snake.new(RacingSnakes::PlayerIds::PLAYER_ONE)
+      @player2 = RacingSnakes::Snake.new(RacingSnakes::PlayerIds::PLAYER_TWO)
+      @board = RacingSnakes::Board.new(@player1, @player2)
     end
 
     def move; end
