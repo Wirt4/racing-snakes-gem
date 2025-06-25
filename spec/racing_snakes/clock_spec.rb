c94d53dc48e4cc589ed16c846a35223dadcff2a4
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-25 16:38:59 -0700

    red

--- spec/racing_snakes/clock_spec.rb
+++ spec/racing_snakes/clock_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe RacingSnakes::Clock do
     it 'clock defaults with time at 0' do
       expect(clock.time).to eq(0)
     end
+    it 'clock will initialized with loaded time' do
+      loaded_clock = described_class.new(7)
+      expect(loaded_clock.time).to eq(7)
+    end
   end
 end
