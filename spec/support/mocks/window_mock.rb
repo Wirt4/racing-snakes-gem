051697839febfc0a12570c7b9e41911a814d648b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-24 19:15:58 -0700

    lint

--- spec/support/mocks/window_mock.rb
+++ spec/support/mocks/window_mock.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable RSpec/ContextWording
 RSpec.shared_context 'with a window mock' do
   let(:mock_window) { instance_double(Ruby2D::Window, set: nil, clear: nil) }
 end
