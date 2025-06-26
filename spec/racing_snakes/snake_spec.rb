e78190a63708e86e3453cd1db83b3274af3be990
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-25 17:33:28 -0700

    refactor

--- spec/racing_snakes/snake_spec.rb
+++ spec/racing_snakes/snake_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe RacingSnakes::Snake do
   
   describe '#initialize' do
-    subject(:snake) { described_class.new('player1') }
+    subject(:snake) { described_class.new(RacingSnakes::PlayerIds::PLAYER_ONE) }
     it 'direction is initialized as up' do
       expect(snake.direction).to eq(RacingSnakes::Directions::UP)
     end
