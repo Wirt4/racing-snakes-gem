f9207df70a60880425e5fc65e11f927c47c3edde
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-28 19:31:10 -0700

    main works

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 require 'ruby2d'
-load 'board.rb'
-load 'game_clock.rb'
-load 'snake.rb'
-load 'settings.rb'
-load 'keyboard_buttons.rb'
-load 'game.rb'
+require_relative 'lib/racing_snakes_gem/board'
+require_relative 'lib/racing_snakes_gem/game'
+require_relative 'lib/racing_snakes_gem/game_clock'
+require_relative 'lib/racing_snakes_gem/settings'
+require_relative 'lib/racing_snakes_gem/snake'
+require_relative 'lib/racing_snakes_gem/keyboard_buttons'
 
 set background: Settings::BACKGROUND
 set width: Settings::WIDTH
@@ -12,34 +12,26 @@ set height: Settings::HEIGHT
 set fps_cap: Settings::FPS
 set fullscreen: Settings::FULLSCREEN
 
-game = Game.new()
+game = Game.new
 game.draw_snakes
 
 # the game cycle
 update do
   clear
 
-  unless game.is_paused?
-   game.move
-  end
+  game.move unless game.is_paused?
 
   game.draw_board
 
-  if game.player_eats
-    game.eat_and_grow
-  end
+  game.eat_and_grow if game.player_eats
 
-  if game.food_time?
-    game.respawn_food
-  end
+  game.respawn_food if game.food_time?
 
-# prints out the winner of the game if it ends
-  if game.is_collision?
-    game.stop_game
-  end
+  # prints out the winner of the game if it ends
+  game.stop_game if game.is_collision?
 end
 
-#player controls, is up, down, left, right /AWSD
+# player controls, is up, down, left, right /AWSD
 on :key_down do |event|
   k = event.key
   game.detect_key(k)
