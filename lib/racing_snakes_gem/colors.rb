5cc94de740d7f95c6b1e71ee88293a0107973b39
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-29 00:12:06 -0700

    docs

--- lib/racing_snakes_gem/colors.rb
+++ lib/racing_snakes_gem/colors.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# This module defines a set of color constants that can be used throughout the application.
+# Each color is represented as a string.
+# The constants are defined using the `||=` operator to ensure they are only set once.
 module Colors
   YELLOW ||= 'yellow'
   ORANGE ||= 'orange'
